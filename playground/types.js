fe9a57f61fa04ebb535a7df9c4084f0c26f28331
nezianyajc <jnezia1@hotmail.com>
2021-09-07 19:16:59 -0400

    Initial Commit
    Worked through first module on the text editor setup and JS types. I did a few small exercises, but I didn't do anything too technical.

---
+++ playground/types.js
@@ -0,0 +1,25 @@
+
+
+const name = 'Josh';
+const middle = "Chukwuka";
+const last = `Nezianya`;
+
+
+let stringText = 'My name is '+ name + ' '+ last;
+console.log(stringText);
+
+stringText = `My middle name is ${middle} `;
+console.log(stringText);
+const age = 31;
+const a = 20;
+const b = 10;
+
+//objects 
+
+const person = {
+    first: "Josh",
+    last: "Nezianya",
+    age: 31
+}
+
+let dog
