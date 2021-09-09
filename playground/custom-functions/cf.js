12a6621ea1cf5ae54bc02bc027e099a735c2c937
nezianyajc <jnezia1@hotmail.com>
2021-09-08 21:08:00 -0400

    Worked through parameters & arguments
    Worked through parameters and arguments section. Learned about default parameters and arguments.

--- playground/custom-functions/cf.js
+++ playground/custom-functions/cf.js
@@ -1,11 +1,23 @@
 
-function calculateBill(){
+
+
+function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15){
     //  this is the function body
     console.log('The function is running');
-    const total = 100 * 1.13;
+    const total = billAmount + (billAmount * taxRate) + (billAmount * tipRate);
     return total;
 }
 //Function Call
-const myTotal = calculateBill();
+const myTotal = calculateBill(100, 0.13);
+const myTotal2 = calculateBill(200, 0.13);
 
-console.log(`My total is $${myTotal}`);
+// console.log(`My total is $${myTotal}`);
+// console.log(`My total is $${myTotal2}`);
+
+function doctorize(name) {
+    return `Dr. ${name}`
+}
+
+function yell(name = 'Default') {
+    return `HEY ${name.toUpperCase()}`;
+}
