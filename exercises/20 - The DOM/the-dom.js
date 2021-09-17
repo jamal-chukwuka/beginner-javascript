3908a0aa4770cb47127efc2fecaa77278845cf2f
nezianyajc <80351494+N-Jadaka@users.noreply.github.com>
2021-09-17 06:06:54 -0400

    Moved through selecting elements section.

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,7 @@
+const p = document.querySelector('p');
+const divs = document.querySelectorAll('div');
+const imgs = document.querySelectorAll('.item img');
+const item2 = document.querySelector('.item2');
+
+console.log(item2);
+
