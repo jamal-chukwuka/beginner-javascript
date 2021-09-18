341012da7740106bcf4fea02393f0b887f8fc937
nezianyajc <80351494+N-Jadaka@users.noreply.github.com>
2021-09-18 00:43:06 -0400

    Finished working on element properties section

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -2,6 +2,21 @@ const p = document.querySelector('p');
 const divs = document.querySelectorAll('div');
 const imgs = document.querySelectorAll('.item img');
 const item2 = document.querySelector('.item2');
+const item2Image = document.querySelector('img');
+const heading = document.querySelector('h2');
 
-console.log(item2);
+console.log(heading.textContent);
+console.log(heading.innerText);
 
+// heading.textContent = 'Josh is dope!';
+// console.log(heading.textContent);
+// console.log(heading.innerText);
+
+console.log(heading.innerHTML);
+console.log(heading.outerHTML);
+ 
+const pizzaList = document.querySelector('.pizza');
+console.log(pizzaList.textContent);
+
+// pizzaList.textContent = `${pizzaList.textContent} + pizza`;
+pizzaList.insertAdjacentElement('beforeend',  'pizza');
