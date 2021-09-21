8cf919d0d500aa439c55bf5131e147b21d97f481
nezianyajc <80351494+N-Jadaka@users.noreply.github.com>
2021-09-21 06:59:05 -0400

    Added a toggle function

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -25,6 +25,12 @@
 const pic = document.querySelector('.nice');
 pic.classList.add('open');
 pic.classList.remove('cool');
-pic.classList.toggle('round');
+
+
 console.log(pic.classList);
 
+function toggleRound() {
+    pic.classList.toggle('round');
+}
+
+pic.addEventListener('click', toggleRound);
